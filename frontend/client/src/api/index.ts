18fa0355a3f2c8426a8ba9424ae23129d087d9db
Рамиль Галиев <rgaliev2004@gmail.com>
2024-05-23 22:11:22 +0300

    #0016.3

--- frontend/client/src/api/index.ts
+++ frontend/client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "http://147.45.158.13:8000"
 });
 
 api.interceptors.request.use(
--- frontend/client/src/config.ts
+++ frontend/client/src/config.ts
@@ -1,4 +1,4 @@
-const WS_URL: string = 'ws://147.45.158.13'
+const WS_URL: string = 'ws://147.45.158.13:8000'
 
 export {
   WS_URL
